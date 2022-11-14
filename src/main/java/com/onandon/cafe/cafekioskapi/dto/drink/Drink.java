84c169fecfe59ce287665585126d1119709bb261
yuno1111 <108105634+yuno1111@users.noreply.github.com>
2022-11-14 13:54:45 +0900

    DrinkMenu 추가

--- src/main/java/com/onandon/cafe/cafekioskapi/dto/drink/Drink.java
+++ src/main/java/com/onandon/cafe/cafekioskapi/dto/drink/Drink.java
@@ -1,7 +1,10 @@
 package com.onandon.cafe.cafekioskapi.dto.drink;
 
 import com.onandon.cafe.cafekioskapi.dto.item.OrderItem;
+import lombok.Data;
 
+@Data
 public class Drink extends OrderItem {
-
+    private String milk;
+    private String ice;
 }
--- src/main/java/com/onandon/cafe/cafekioskapi/utils/OrderUtils.java
+++ src/main/java/com/onandon/cafe/cafekioskapi/utils/OrderUtils.java
@@ -11,23 +11,37 @@ import java.util.Map;
 
 public final class OrderUtils {
     public static Map<String,Object> getOrder(Items item){
-        int index = 0;
+        int coffeeIndex = 0;
+        int drinkIndex = 0;
         Map<String,Object> resultMap = new HashMap<>();
         List<Coffee> coffees = new ArrayList<>();
         List<Drink> drinks = new ArrayList<>();
 
         for(Coffee coffee: item.getCoffees()){
-            for(int i=0; i<item.getCoffees().get(index).getCount(); i++){
-                coffee.setName(item.getCoffees().get(index).getName());
-                coffee.setShot(item.getCoffees().get(index).getShot());
-                coffee.setMilk(item.getCoffees().get(index).getMilk());
-                coffee.setIce(item.getCoffees().get(index).getIce());
-                coffee.setTumbler(item.getCoffees().get(index).getTumbler());
+            for(int i=0; i<item.getCoffees().get(coffeeIndex).getCount(); i++){
+                coffee.setName(item.getCoffees().get(coffeeIndex).getName());
+                coffee.setShot(item.getCoffees().get(coffeeIndex).getShot());
+                coffee.setMilk(item.getCoffees().get(coffeeIndex).getMilk());
+                coffee.setIce(item.getCoffees().get(coffeeIndex).getIce());
+                coffee.setTumbler(item.getCoffees().get(coffeeIndex).getTumbler());
                 coffees.add(coffee);
             }
-            index ++;
+            coffeeIndex ++;
         }
+
+        for(Drink drink: item.getDrinks()){
+            for(int i=0; i<item.getDrinks().get(drinkIndex).getCount(); i++){
+                drink.setName(item.getDrinks().get(drinkIndex).getName());
+                drink.setMilk(item.getDrinks().get(drinkIndex).getMilk());
+                drink.setIce(item.getDrinks().get(drinkIndex).getIce());
+                drink.setTumbler(item.getDrinks().get(drinkIndex).getTumbler());
+                drinks.add(drink);
+            }
+            drinkIndex ++;
+        }
+
         resultMap.put("coffees",coffees);
+        resultMap.put("drink",drinks);
         return resultMap;
     }
 }
